0d47597562ffe96dafcc8b44323c609412355511
ioctaptceb <yulia.startsev@gmail.com>
2016-07-08 15:53:46 +0200

    fuf

--- src/node_pdfium.cc
+++ src/node_pdfium.cc
@@ -575,8 +575,8 @@ MY_NODE_MODULE_CALLBACK(render)
   v8::String::Utf8Value outputFormatObject(options->Get(V8_STRING_NEW_UTF8("outputFormat"))->ToString());
   v8::Local<v8::ArrayBuffer> dataobject = options->Get(V8_STRING_NEW_UTF8("data")).As<v8::ArrayBuffer>();
 
-  req->data.assign(static_cast<char*>(dataobject.Externalize().Data()),
-          dataobject.Externalize().ByteLength());
+  req->data.assign(static_cast<char*>(dataobject->Externalize().Data()),
+          dataobject->Externalize().ByteLength());
 //  else
 //  {
 //    RETURN_EXCEPTION_STR_CB("data must be a Buffer", callback);
